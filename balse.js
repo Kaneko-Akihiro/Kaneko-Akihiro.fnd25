ededbbcd714f7d04f4dd3f438d6d1e65c094cd13
Kaneko-Akihiro <158393234+Kaneko-Akihiro@users.noreply.github.com>
2024-03-23 14:45:48 +0900

    Create balse.js

---
+++ balse.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="./balse.css" rel="stylesheet" />
+  <script defer src="./balse.js"></script>
+  <title>バルス！！</title>
+</head>
+<body>
+  <button id=balse >バルス</button>
+  <div class="img">
+    <img id="balsed" class="balsed" src="balse.jpg">
+    <img id="balsed2" class="balsed2" src="balse2.jpg">
+    <img id="thankyou" class="thankyou" src="thankyou.png">
+  </div>
+</body>
+</html>
